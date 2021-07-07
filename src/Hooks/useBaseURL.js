25c92ca47403874638d1768f5ee24065fda4d96c
fjvigil89 <frankjosue.vigilvega@gmail.com>
2021-07-07 09:54:20 +0200

    Cambio de BaseURL API

--- src/Hooks/useBaseURL.js
+++ src/Hooks/useBaseURL.js
@@ -3,8 +3,8 @@ import {useState} from "react";
 const useBaseURL = (urlApi) => {
   //const [url, setUrl] = useState("http://192.168.1.44:8000/"); 
   //const [url, setUrl] = useState("http://155.210.158.136:8000/");
-  const [url, setUrl] = useState("http://localhost:8000/");
-  //const [url, setUrl] = useState("http://155.210.153.12:8080/");
+  //const [url, setUrl] = useState("http://localhost:8000/");
+  const [url, setUrl] = useState("http://155.210.153.12:8080/");
   if(urlApi !== null){
     setUrl(urlApi);
   }  
