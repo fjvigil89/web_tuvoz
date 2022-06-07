66adce751ceb7030074409422f1c1a11d7b47339
fjvigil89 <frankjosue.vigilvega@gmail.com>
2022-06-07 14:22:45 +0200

    agregando paginación y download dataset

--- src/components/Specialist/BucketS3/BucketS3TableList.js
+++ src/components/Specialist/BucketS3/BucketS3TableList.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-
 import {
   Card,
   CardHeader,
@@ -11,6 +10,8 @@ import {
   Table,
   Container,
   Row,
+  Button,
+  Badge,
 } from "reactstrap";
 import Cookies from "universal-cookie";
 import axios from "axios";
@@ -32,16 +33,15 @@ const params = {
   
 } 
 
-
 const s3Bucket= new AWS.S3(params);
 
 
 const BucketS3 = () => {
   //uso del Hooks para la url de la API
   const baseURL = useBaseURL(null);
+  const hostS3= "https://tuvoz-bucket.s3.eu-central-1.amazonaws.com/";
   const formatter = new useFormatterDate();
-
-
+  
   //gusrdar los tratamientos del Usuario Logueado
   const [record, setRecord] = useState([
     {
@@ -54,7 +54,20 @@ const BucketS3 = () => {
     },
  
   ]);
-  
+  const [users, setUsers] = useState([ ]);
+  //paginations
+  const [currentPage, setCurrentPage] = useState(0);
+  const [pageSize, setPageSize] = useState(0);
+  const [pagesCount, setPagesCount] = useState(0);
+
+  const filterArray=(inputArr)=>{
+    var found ={};
+    var out = inputArr.filter(function(element){
+        return found.hasOwnProperty(element)? false : (found[element]=true);
+    });    
+    return out;
+  }
+
   const listObjectsInBucket = async () => {
 
     // Call S3 to obtain a list of the objects in the bucket
@@ -62,8 +75,9 @@ const BucketS3 = () => {
       if (err) {
         console.log("Error", err);
       } else {
-
+        
         let aux = Array();
+        let auxUser = Array();
         data.Contents.map((element, index) => {
           if (index % 2 === 0) {
             aux.push({
@@ -72,38 +86,57 @@ const BucketS3 = () => {
               date: formatter.format(Date.parse(element.LastModified)) ,
               size:element.Size + data.Contents[index+1].Size,
             });
+            auxUser.push(element.Key.split("(")[0]);                     
           }
-          
-          
         });
-        setRecord(aux)
+        setRecord(aux);
+        setUsers(filterArray(auxUser));              
+        const porcentPage = 0.1; //10%
+        setPageSize(Math.ceil(aux.length * porcentPage));
+        setPagesCount(Math.ceil(aux.length / (aux.length * porcentPage)));                
       }
     }); 
 
   }
 
-  const downloadS3 = (item)=>{
+  const downloadS3 = async(item)=>{    
+    
+    let resp = await fetch(hostS3+item.metadato);
+    let body = await resp.blob();      
+    let url = window.URL.createObjectURL(body);
+    let tempLink = document.createElement('a');
+    tempLink.href = url;
+    tempLink.setAttribute('download', item.metadato);
+    tempLink.click();
 
-    s3Bucket.getObject({Bucket:params.Bucket, Key: item}, (err, data) => {
-      if (err) {
-        console.log(err, err.stack);
-      } else {
-        const json = JSON.stringify(data.Body.toString());
-        console.log(json);
-        //console.log(data.Body.toString());
-        
-      }
-    });
+    resp = await fetch(hostS3+item.record);
+    body = await resp.blob();      
+    url = window.URL.createObjectURL(body);
+    tempLink = document.createElement('a');
+    tempLink.href = url;
+    tempLink.setAttribute('download', item.record);
+    tempLink.click();
   }
 
-  useEffect(() => {
+  const downloadDataset = async()=>{    
+    record.map((item) => {
+       downloadS3(item);
+  });
     
-    listObjectsInBucket()
+  }
 
+  const handlePageClick = (e, index) => {
+    e.preventDefault();
+    setCurrentPage(index);
+ };
+
+  useEffect(() => {
+    listObjectsInBucket()    
 
     if (!cookie.get("token")) {
       window.location.href = "/auth/login";
     }
+    
   }, []);
 
   return (
@@ -119,6 +152,7 @@ const BucketS3 = () => {
                     <span className="mb-0 text-sm">
                       <h3 className="mb-0">Grabaciones</h3>
                     </span>
+                    <Badge color="primary" >{record.length}</Badge>
                   </Media>
                 </Media>
               </CardHeader>
@@ -126,6 +160,7 @@ const BucketS3 = () => {
               <Table className="align-items-center table-flush" responsive>
                 <thead className="thead-light">
                   <tr>
+                    <th scope="col">#</th>
                     <th scope="col">Metadatos</th>
                     <th scope="col">Audio</th>
                     <th scope="col">Fecha</th>
@@ -134,16 +169,20 @@ const BucketS3 = () => {
                   </tr>
                 </thead>
                 <tbody>
-                  {record.map((item, index) => (                    
+                  {record.slice(currentPage * pageSize, (currentPage + 1) * pageSize ).map((item, index) => (                    
                     <>
                       <tr key={index}>
-                        <th scope="row">
-                        <div className="avatar-group" onClick={()=>downloadS3(item.metadato)} >
-                            {item.metadato}
-                          </div>
+                        <th scope="row" onClick={()=>downloadS3(item)} >
+                          <i className="fas fa-angle-down" />
+                          <span className="sr-only">Previous</span>
                         </th>
                         <td>
-                          <div className="avatar-group" onClick={()=>downloadS3(item.record)}>
+                        <div className="avatar-group" >
+                            {item.metadato}
+                          </div>
+                        </td>
+                        <td>
+                          <div className="avatar-group" >
                             {item.record}
                           </div>
                         </td>
@@ -165,48 +204,39 @@ const BucketS3 = () => {
               </Table>
               <CardFooter className="py-4">
                 <nav aria-label="...">
+                <Button className="btn-icon btn-3" color="primary" type="button" onClick={()=>downloadDataset()}>
+                  <span className="btn-inner--icon">
+                    <i className="ni ni-bag-17" />
+                  </span>
+                  <span className="btn-inner--text">DataSet</span>
+                </Button>
+                </nav>
+                <nav aria-label="...">
                   <Pagination
                     className="pagination justify-content-end mb-0"
                     listClassName="justify-content-end mb-0"
                   >
-                    <PaginationItem className="disabled">
+                    <PaginationItem disabled={currentPage <= 0}>
                       <PaginationLink
                         href="#pablo"
-                        onClick={(e) => e.preventDefault()}
+                        onClick={e => handlePageClick(e, currentPage - 1)}
                         tabIndex="-1"
                       >
                         <i className="fas fa-angle-left" />
                         <span className="sr-only">Previous</span>
                       </PaginationLink>
                     </PaginationItem>
-                    <PaginationItem className="active">
+                    {[...Array(pagesCount)].map((page, i) => 
+                      <PaginationItem active={i === currentPage} key={i}>
+                        <PaginationLink onClick={e => handlePageClick(e, i)} href="#">
+                          {i + 1}
+                        </PaginationLink>
+                      </PaginationItem>
+                    )}                                          
+                    <PaginationItem disabled={currentPage >= pagesCount - 1}>
                       <PaginationLink
                         href="#pablo"
-                        onClick={(e) => e.preventDefault()}
-                      >
-                        1
-                      </PaginationLink>
-                    </PaginationItem>
-                    <PaginationItem>
-                      <PaginationLink
-                        href="#pablo"
-                        onClick={(e) => e.preventDefault()}
-                      >
-                        2 <span className="sr-only">(current)</span>
-                      </PaginationLink>
-                    </PaginationItem>
-                    <PaginationItem>
-                      <PaginationLink
-                        href="#pablo"
-                        onClick={(e) => e.preventDefault()}
-                      >
-                        3
-                      </PaginationLink>
-                    </PaginationItem>
-                    <PaginationItem>
-                      <PaginationLink
-                        href="#pablo"
-                        onClick={(e) => e.preventDefault()}
+                        onClick={e => handlePageClick(e, currentPage + 1)}
                       >
                         <i className="fas fa-angle-right" />
                         <span className="sr-only">Next</span>
@@ -222,4 +252,5 @@ const BucketS3 = () => {
     </>
   );
 };
+
 export default BucketS3;
